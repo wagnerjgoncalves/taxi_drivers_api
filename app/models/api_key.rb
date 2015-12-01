445dfc40b611f571efa0afa7922044a824d291a6
Wagner J Goncalves <wagnerjgoncalves@gmail.com>
2015-11-30 22:23:04 -0200

    Up

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -2,7 +2,7 @@ class APIKey < ActiveRecord::Base
   def self.generate
     loop do
       token = SecureRandom.base64.tr('+/=', 'Qrt')
-      break token unless APIKey.exists?(key: token).any?
+      break token unless APIKey.exists?(token: token)
     end
   end
 end
